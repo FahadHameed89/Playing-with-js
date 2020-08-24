25ee5468a79afa045804c2a754eff956ed3f2f47
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-24 12:08:20 -0600

    Added listeners, next - How to get 'value' from form field, how to make multiple other list elements, how to insert text into LI element. How do add LI element to the page or list?

--- js/dom-events-form.js
+++ js/dom-events-form.js
@@ -5,9 +5,21 @@
 const helloForm = document.querySelector('form');
 console.log(helloForm);
 
-// 
+// This grabs the name field
 const nameField = document.getElementById('your-name');
 console.log(nameField);
 
+// This grabs the actual list output
 const helloList = document.querySelector('#hello-list');
 console.log(helloList) ;
+
+/**
+ * Event Listener: Form Submit
+ */
+
+ // Event Listeners pass an argument to our function, an event object
+helloForm.addEventListener ('submit', ( event )=> { 
+    event.preventDefault(); // This stops a form from a default submission
+    console.log (event); // This logs whatever is under Event
+    console.log (event.target); // This logs the element that the event occured on.
+} );
