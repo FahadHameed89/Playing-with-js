924b0d75c02df4e293f6a43a93d91e7a7b41a442
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-24 11:51:21 -0600

    Successfully captured various values for our form list

--- js/dom-events-form.js
+++ js/dom-events-form.js
@@ -1,5 +1,13 @@
 // alert ("DOM Events Form Script Loaded...!")
 // Script Loads correctly...!
 
+// We can grab our form, and get it to 'listen' for 'Submit!'
 const helloForm = document.querySelector('form');
 console.log(helloForm);
+
+// 
+const nameField = document.getElementById('your-name');
+console.log(nameField);
+
+const helloList = document.querySelector('#hello-list');
+console.log(helloList) ;
