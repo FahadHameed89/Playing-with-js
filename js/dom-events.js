558183137149379f14cb0d78c77746a1a8dd8038
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-24 09:49:08 -0600

    Added some non-functioning buttons

--- js/dom-events.js
+++ js/dom-events.js
@@ -1,3 +1,4 @@
 // alert("Are you there?") -- Use Alerts to verify that JS is operating...!
 // console.log("Test to see if the JS is running") -- Console logs work too...!
 
+// You can use the # in an href call to travel to a specific subheading in a page. 
