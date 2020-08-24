65400544b6b06328a37c129c0cd5f59da851e4ad
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-24 10:59:49 -0600

    Added mouse hover and leave functions to test button

--- js/dom-events.js
+++ js/dom-events.js
@@ -1,74 +1,65 @@
-// alert("Are you there?") -- Use Alerts to verify that JS is operating...!
-// console.log("Test to see if the JS is running") -- Console logs work too...!
+// First thing's first... is our file loading into the browser?!
+// alert( "DOM Events JS loaded!" );
+// console.log( "DOM Events JS loaded!" );
+// ***Normally the above gets removed after your test...
+// ***This is just for your reference!
 
-// You can use the # in an href call to travel to a specific subheading in a page. 
+/**
+ * Targeting via the DOM (Document Object Model) in JavaScript
+ */
 
-
-/*
-    Targetting via the DOM (Document Object Model) in JavaScript!
-*/
-
-// We want to target hte h2 with the id="target-me"
-
+// Grab our H2 as practice.
 const myH2 = document.getElementById( 'target-me' );
-console.log( myH2 ); // This successfully grabs the element!
+console.log( myH2 ); // We can see in console we successfully retrieved this element!
 
-// Now lets see if we can grab an entire paragraph!
+// Let's try and grab the proceeding paragraph...
+const myP = document.querySelector( '#target-me + p' ); // Target a <p> DIRECTLY AFTER the element with id="target-me"
+console.log( myP );
 
-// This should collect the first match that is a paragraph 'p'
-// You can also add ALL to query selector to see all 
-// const myP = document.querySelectorALL( 'p' );
+// Let's grab our hover button!
+const hoverButton = document.querySelector( 'p > button' ); // Remember... this grabs the FIRST match only.
+console.log( hoverButton );
 
-// const myP = document.querySelector( '#target-me + p' ); - Will pull the first p neighboring #target-me, nothing in #target-me. In order for + to target something, it must be ADJACENT to the first operator - in this case, this will only target a <p> located adjacent to anything with a #target-me ID. This only occurs once in the entire program, and therefore is a good way to target a specific element. 
+// Grab our first span in the <p> (second child, in this case.)
+const firstSpan = document.querySelector( 'p > span:nth-child(2)' );
+console.log( firstSpan );
 
-const myP = document.querySelector( '#target-me + p' );
+// Let's grab our hover button!
+const clickButton = document.querySelector( 'p > button:nth-of-type(2)' );
+console.log( clickButton );
 
-console.log(myP);
-
-const hoverButton = document.querySelector( 'p > button' );
-
-console.log(hoverButton);
-
-//const firstSpan = document.querySelector( 'p > span:nth-of-child(2)' )
-// console.log(firstSpan);
-
-const clickButton = document.querySelector( 'p > button:nth-of-type(1)' );
-console.log(clickButton);
-// This will select the 2nd type of button that is within the first paragraph
-
-// const secondSpan = document.querySelector( 'p > span:nth-of-child(4)' )
-// console.log(secondSpan);
-// This will grab the 4th child descendant of the first p element, in this case, the second span of text. 
+// Grab the second span in the <p> (fourth child, in this case.)
+const secondSpan = document.querySelector( 'p > span:nth-child(4)' );
+console.log( secondSpan );
 
 /**
- *      DOM Element Manipulation
- * 
+ * DOM / Element Manipulation
  */
 
-// We can use JS to update / replace content!
- myH2.textContent = "DOM Target and Element Adjustment Practice!";
+// We can use JS to update / replace content in elements.
+myH2.textContent = "This H2 Was Found and Updated!";
+console.log( 'Outputted string into our <h2> element!' );
+// We are replacing the existing CSS string.
+myH2.className = 'salmon-coloured-text';
+console.log( 'Change the H2 font colour to "salmon."' );
 
- console.log( 'Outputted striing into our <h2> element!');
-
- myH2.className = 'salmon-coloured-text';
-
-
- /**
-  *     EVENT LISTENERS
-  *     We can add 'listeners' to elements, which listen for user actions.
-  *     If a user action is detected, we can have our JS follow a set of instructions!
-  */
-
-  // This is how we add an event listener to a specific variable...
 /**
- * There are two arguments for addEventListener:
- * 1. Type of Event (by name)
- * 2. The instructions (what should run when the event occurs!)
- * 
- *  (This means: a function!)
+ * Event Listeners.
+ * We can add "listeners" to elements... they "listen" for user actions.
+ * When a user action is detected, we can have our JS follow a set of instructions.
  */
 
-  hoverButton.addEventListener( 'mouseenter', () => {
-      console.log( 'The Mouse has entered the button!' );
+// We can add event listeners to HTMLElement objects.
+/**
+ * There are two arguments to the addEventListener method:
+ *   1) The type of event (by name.)
+ *   2) The instructions (what should run when the event occurs.)
+ *      (This means: a function!)
+ */
+hoverButton.addEventListener( 'mouseenter', () => {
+  console.log( 'The mouse has entered the button!' );
+} );
+
+hoverButton.addEventListener( 'mouseleave', () => {
+    console.log( 'The mouse has left the button!' );
   } );
-  
