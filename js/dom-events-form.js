ce270b61c57fb5e4478afb53411f315daaad9ec7
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-24 13:35:19 -0600

    Added innerHTML example

--- js/dom-events-form.js
+++ js/dom-events-form.js
@@ -17,8 +17,37 @@ console.log(helloList) ;
  * Event Listener: Form Submit
  */
 
+//  // Event Listeners pass an argument to our function, an event object
+// helloForm.addEventListener ('submit', ( event )=> { 
+//     event.preventDefault(); // This stops a form from a default submission
+//     console.log (event); // This logs whatever is under Event
+//     console.log (event.target); // This logs the element that the event occured on.
+
+// // How to get our 'value' from our form field?
+// const name = nameField.value;
+// console.log( name) ;
+
+// // How do i make a new List element?
+// const newLI = document.createElement( 'LI' ); // We want to create a <li> element
+// console.log( newLI ); // Notice, it's not actually on the page yet, but it exists since we can see it on the console log...
+// // We must now give instructions on where and how to add it to the page.
+
+// // How do insert a value/text into the List element?
+// newLI.textContent = `Hello, ${name}!`;      // Adds content to the list element...
+// console.log( newLI.textContent ); // Test
+// console.log( newLI);
+
+// // How do I add the LI element to the page (or rather, to our list???)
+// helloList.appendChild( newLI );     // This will insert a CHILD into the HELLOLIST element, thild will be newLI, whatever we specified there...
+// // you can appendChild to lots of different places, moving the variable as you wish.
+// // insert li inside ul
+
+
+/**
+ *  innerHTML example
+ */
  // Event Listeners pass an argument to our function, an event object
-helloForm.addEventListener ('submit', ( event )=> { 
+ helloForm.addEventListener ('submit', ( event )=> { 
     event.preventDefault(); // This stops a form from a default submission
     console.log (event); // This logs whatever is under Event
     console.log (event.target); // This logs the element that the event occured on.
@@ -28,19 +57,8 @@ const name = nameField.value;
 console.log( name) ;
 
 // How do i make a new List element?
-const newLI = document.createElement( 'LI' ); // We want to create a <li> element
-console.log( newLI ); // Notice, it's not actually on the page yet, but it exists since we can see it on the console log...
-// We must now give instructions on where and how to add it to the page.
+helloList.innerHTML += `<li>Hello, ${name}! (innerHTML Version!)</li>`;
 
-// How do insert a value/text into the List element?
-newLI.textContent = `Hello, ${name}!`;      // Adds content to the list element...
-console.log( newLI.textContent ); // Test
-console.log( newLI);
-
-// How do I add the LI element to the page (or rather, to our list???)
-helloList.appendChild( newLI );     // This will insert a CHILD into the HELLOLIST element, thild will be newLI, whatever we specified there...
-// you can appendChild to lots of different places, moving the variable as you wish.
-// insert li inside ul
 
 } );
 
