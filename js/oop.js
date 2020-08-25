2e90cf688b506704db3533d8056d9f6494cb68df
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-25 09:45:23 -0600

    Show how to access properties within an object

--- js/oop.js
+++ js/oop.js
@@ -1,2 +1,16 @@
 // alert("oop.js has been loaded!")
 
+/**
+ *  Objects are marked by curly braces
+ */
+
+ const myObj = {
+     name: "Fahad",
+     age: 30,
+     class: "Knight",
+     Traits: [ "Heroic Strength", "Sword Mastery" ]
+ };
+
+// In order to access properties within an object, use the . modifier, to get the name!
+ console.log(`My name is ${myObj.name}. I am ${myObj.age}, and I am a ${myObj.class}`);
+
