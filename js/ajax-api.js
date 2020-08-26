1054dfba046332d8943339e17aa33ae605c7f173
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-08-26 10:48:57 -0600

    Initial AJAX notes

--- js/ajax-api.js
+++ js/ajax-api.js
@@ -1,1 +1,16 @@
-alert( "DOM Events JS loaded!" );
+// alert( "AJAX-API JS loaded!" );
+
+/**
+ * AJAX (Asynchronous JavaScript and XML)
+ * =============================================================================================
+ * Lets us use Javascript to request information from spearate URLS
+ * Make separate HTTP/HTTPS request post initial page-load.
+ * We can use JS to make an AJAX request to URL endpoints to request additional data
+ * You can retrive HTML, JSON, or XML. Mostly JSONs nowadays because it's really good for this type of thing. 
+ * 
+ * Despite AJAX referring to XML, we are usually dealing with JSON formatted data.
+ * 
+ * With JS, this is our best option for making external requests and getting data returned.
+ * 
+ * The modern way to make AJAX requests with JS is to use the built-in 'Fetch' function/feature.
+ */
